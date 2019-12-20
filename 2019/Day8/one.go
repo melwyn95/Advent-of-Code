7defcfaa1487ff2d4c2ae70700e9d290be1e212b
melwyn95 <melwyn95@gmail.com>
2019-12-20 22:25:19 +0530

    Solve Day 8 problem 1

---
+++ 2019/Day8/one.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func toIntSlice(s string) []int {
+	l := len(s)
+	intSlice := make([]int, l)
+	for i := 0; i < l; i++ {
+		intSlice[i] = int(s[i] - '0')
+	}
+	return intSlice
+}
+
+func count(slice []int, x int) int {
+	count := 0
+	for i := range slice {
+		if slice[i] == x {
+			count++
+		}
+	}
+	return count
+}
+
+func Solve1(image string, row, col int) {
+	imagelen, layerlen, lidx := len(image), row*col, 0
+	layers := make([]string, imagelen/layerlen)
+	for i := 0; i < imagelen; i += layerlen {
+		layers[lidx] = image[i : i+layerlen]
+		lidx++
+	}
+	min, minrow := -1, -1
+	for i := range layers {
+		c := count(toIntSlice(layers[i]), 0)
+		if min == -1 {
+			min = c
+			minrow = i
+		} else if c < min {
+			min = c
+			minrow = i
+		}
+	}
+	fmt.Println(count(toIntSlice(layers[minrow]), 1) * count(toIntSlice(layers[minrow]), 2))
+}
