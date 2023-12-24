2be6f5c62075b34681a7aa236e001180348fe6ef
Melwyn Saldanha <melwyn95@gmail.com>
2023-12-24 11:47:23 -0500

    day 05: part1 done

--- aoc2023/day05.ml
+++ aoc2023/day05.ml
@@ -35,7 +35,7 @@ let split lines =
     | line :: maps when String.contains line ':' -> aux maps acc1 acc2
     | line :: maps -> aux maps (line :: acc1) acc2
   in
-  seeds, aux maps [] []
+  seeds, List.tl @@ aux maps [] []
 
 let parse_maps : string list list -> range list list = fun maps ->
   List.map (fun map ->
@@ -48,14 +48,43 @@ let parse_maps : string list list -> range list list = fun maps ->
     ) map []
   ) maps
 
+let find_destination value map =
+  let { destination_start ; source_start ; length } = map in
+  if value < source_start || value >= source_start + length
+  then None
+  else
+    let diff = abs (value - source_start) in
+    Some (destination_start + diff)
+
+let find_destination maps value =
+  match List.find_map (find_destination value) maps with
+  | None -> value
+  | Some value -> value
+
+let find_location seed maps =
+  List.fold_left (fun seed ranges ->
+    find_destination ranges seed  
+  ) seed maps   
+
+let find_min_location seeds maps =
+  match seeds with
+  | seed :: seeds ->
+    let location = find_location seed maps in
+    List.fold_left (fun min_loc seed -> 
+      min min_loc (find_location seed maps)
+    ) location seeds
+  | _ -> failwith "empty seeds"
+
 let main () =
   Printf.printf "==== Day 05 ====\n";
-  let input = "inputs/day05_simpl.txt" in
-  (* let input = "inputs/day05.txt" in *)
+  (* let input = "inputs/day05_simpl.txt" in *)
+  let input = "inputs/day05.txt" in
   let lines = read_file input in
   let seeds, maps = split lines in
   let maps = parse_maps maps in
-  List.iter (List.iter (print_range)) maps;
+  let location = find_min_location seeds maps in
+  Printf.printf "Part1> %d\n" location;
+  (* List.iter (fun rs -> if rs = [] then Printf.printf "[]\n";List.iter (print_range) rs) maps; *)
   ()
 
 let () = main ()
