e3a00234850044889a8fb387d6a391a76395d61f
Melwyn Saldanha <melwyn95@gmail.com>
2023-12-24 22:00:01 -0500

    day 06: solve part 2

--- aoc2023/day06.ml
+++ aoc2023/day06.ml
@@ -30,24 +30,33 @@ let parse lines =
     zip time distance
   | _ -> failwith "invalid input"
 
+let parse2 lines = 
+  match lines with
+  | [ time ; distance ] ->
+    let time = List.hd @@ List.tl @@ String.split_on_char ':' time in
+    let time = String.trim time in
+    let time = String.split_on_char ' ' time 
+      |> List.filter (fun s -> s <> "") in
+    let time = List.fold_left String.cat "" time in
+    let distance = List.hd @@ List.tl @@ String.split_on_char ':' distance in
+    let distance = String.trim distance in
+    let distance = String.split_on_char ' ' distance 
+      |> List.filter (fun s -> s <> "") in
+    let distance = List.fold_left String.cat "" distance in
+    int_of_string time, int_of_string distance
+  | _ -> failwith "invalid input"
+
 let rec range start end_ =
   if start > end_
   then []
   else start :: range (start + 1) end_
 
 let beat_record (time, distance) =
-  let rec aux x =
-    if (time - x) * x <= distance
-    then 0
-    else 1 + aux (x - 1)
-  in
-  Printf.printf "? %d\n" (aux (time / 2));
-  2 * aux (time / 2) - if time mod 2 = 0 then 1 else 0
-  (* List.length @@ List.filter_map (fun x ->
-    if (time - x) * x > distance
-    then Some x
-    else None
-  ) (range 0 time) *)
+  let time = float_of_int time in
+  let distance = float_of_int distance in
+  let r1 = time +. (sqrt (time *. time -. 4. *. distance)) /. 2. in
+  let r2 = time -. (sqrt (time *. time -. 4. *. distance)) /. 2. in
+  int_of_float @@ ceil r1 -. ceil r2
 
 let main () =
   Printf.printf "==== Day 06 ====\n";
@@ -59,6 +68,8 @@ let main () =
     e * beat_record round
   ) 1 rounds in
   Printf.printf "Part1> %d\n" error_margin;
+  let time, distance = parse2 lines in
+  Printf.printf "Part2> %d\n" (beat_record (time, distance));
   ()
 
 let () = main ()
