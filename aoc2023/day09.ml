27f4fffa8cf8c329b7a546b2864977e947733e51
Melwyn Saldanha <melwyn95@gmail.com>
2023-12-29 21:18:19 -0500

    day 09: solve part 2

--- aoc2023/day09.ml
+++ aoc2023/day09.ml
@@ -33,6 +33,24 @@ let extrapolate xs =
 let parse line =
   String.split_on_char ' ' line |> List.map int_of_string
 
+let extrapolate2 xs =
+  let rec aux xs =
+    match xs with
+    | [ a ; b ] -> [ b - a ]
+    | a :: b :: xs -> (b - a) :: aux (b :: xs)
+    | _ -> failwith "toto"
+  in
+  let rec while_all_zero xs =
+    if is_all_zero xs
+    then []
+    else
+      let first = List.hd xs in
+      let xs = aux xs in
+      first :: while_all_zero xs
+  in
+  let firsts = while_all_zero xs in
+  List.fold_right ( - ) firsts 0
+
 let main () =
   Printf.printf "==== Day 09 ====\n";
   (* let input = "inputs/day09_simpl.txt" in *)
@@ -40,9 +58,11 @@ let main () =
   let lines = read_file input in
   let steps = List.map parse lines in
   let es = List.map extrapolate steps in
-  (* List.iter (fun n -> Printf.printf "- %d\n" n) es; *)
   let sum_es = List.fold_left ( + ) 0 es in
   Printf.printf "Part1> %d\n" sum_es;
+  let es = List.map extrapolate2 steps in
+  let sum_es = List.fold_left ( + ) 0 es in
+  Printf.printf "Part2> %d\n" sum_es;
   ()
 
 let () = main ()
